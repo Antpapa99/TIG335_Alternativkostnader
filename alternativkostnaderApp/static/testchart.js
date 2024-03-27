728d3b2107ec768c847e7f80c5fc1e3f492ff1dd
Antpapa99 <anthony.oram@outlook.com>
2024-03-27 17:10:17 +0100

    made the chart interactive

--- alternativkostnaderApp/static/testchart.js
+++ alternativkostnaderApp/static/testchart.js
@@ -1,24 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var ctx = document.getElementById('myChart').getContext('2d');
-
-    function fetchChartData() {
-        return fetch('/fetch_chart_data/') // URL to fetch chart data from
-            .then(response => response.json())
-            .then(data => {
-                return data;
-            })
-            .catch(error => {
-                console.error('Error fetching chart data:', error);
-            });
-    }
-
-    fetchChartData().then(chartData => {
-        var myChart = new Chart(ctx, {
+    
+    function renderChart(data) {
+        return new Chart(ctx, {
             type: 'pie',
             data: {
-                labels: chartData.labels,
+                labels: data.labels,
                 datasets: [{
-                    data: chartData.values,
+                    data: data.values,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.5)',
                         'rgba(54, 162, 235, 0.5)',
@@ -35,6 +24,34 @@ document.addEventListener('DOMContentLoaded', function () {
             options: {
                 // Add any options you need here
             }
+        });
+    }
+
+    function fetchChartData() {
+        return fetch('/fetch_chart_data/') // URL to fetch chart data from
+            .then(response => response.json())
+            .catch(error => {
+                console.error('Error fetching chart data:', error);
+            });
+    }
+
+    fetchChartData().then(chartData => {
+        let myChart = renderChart(chartData);
+        let labelAIncrement = 0; 
+
+        // Add event listener to button
+        document.querySelector('button').addEventListener('click', function () {
+            labelAIncrement += 20;
+            fetchChartData().then(updatedChartData => {
+                // Update label A value by 20
+                updatedChartData.values[0] += labelAIncrement
+
+                // Clear previous chart
+                myChart.destroy();
+
+                // Render updated chart
+                myChart = renderChart(updatedChartData);
+            });
         });
     });
 });
