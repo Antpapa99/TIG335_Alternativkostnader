1492cca92c930b39b56cc5e005e12781a767be0f
Antpapa99 <anthony.oram@outlook.com>
2024-04-02 14:10:37 +0200

    Modell ny

--- alternativkostnaderApp/serializers.py
+++ alternativkostnaderApp/serializers.py
@@ -2,9 +2,15 @@ from rest_framework import serializers
 from .models import *
 
 
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = ['tech_name', 'value']
+
+
 
 class CommuneSerializer(serializers.ModelSerializer):
-    technologies = serializers.StringRelatedField(many=True)
+    technologies = TechnologySerializer(many=True)
 
     class Meta:
         model = Commune
