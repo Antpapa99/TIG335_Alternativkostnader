826a6de7b29efca52e8eaa67df2dd226df60b161
Antpapa99 <anthony.oram@outlook.com>
2024-04-02 10:32:36 +0200

    Testing database

--- alternativkostnaderApp/models.py
+++ alternativkostnaderApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Commune(models.Model):
+    commune_name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    
+    
+
+    def __str__(self):
+        return self.name
+
+class CommuneTechnology(models.Model):
+    commune = models.ForeignKey(Commune, on_delete=models.CASCADE)
+    total_installation = models.IntegerField()
+    potential_installation = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.commune.name} - {self.technology.name}"
+
