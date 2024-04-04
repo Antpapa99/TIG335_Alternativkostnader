27ae2195194f970fdadd40c9bcb6ee1025508fbf
Antpapa99 <anthony.oram@outlook.com>
2024-04-04 16:57:39 +0200

    Added a way to prevent duplicate technologies in the dropdown

--- alternativkostnaderApp/static/addRows.js
+++ alternativkostnaderApp/static/addRows.js
@@ -1,7 +1,11 @@
+// Maintain a list of selected technologies
+var selectedTechnologies = [];
+
 function addRow() {
     var table = document.getElementById('tekniktable').getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.rows.length);
     var cells = [];
+
     for (var i = 0; i < 6; i++) {
         cells.push(newRow.insertCell(i));
         if (i === 0) {
@@ -9,6 +13,10 @@ function addRow() {
             select.name = 'teknik';
             select.classList.add('teknikselect');
             var options = ['Välj teknik', 'Digitala lås', 'Digital tillsyn (dag)', 'Digital tillsyn (natt)', 'Läkemedelsrobot', 'Digitalt larm (GPS)', 'Digitalt larm (trygghet)', 'Fallprevention'];
+
+            // Filter out already selected options
+            options = options.filter(optionText => !selectedTechnologies.includes(optionText.toLowerCase().replace(/\s+/g, '')));
+
             options.forEach(function(optionText) {
                 var option = document.createElement('option');
                 option.value = optionText.toLowerCase().replace(/\s+/g, '');
@@ -23,6 +31,19 @@ function addRow() {
             cells[i].appendChild(input);
         }
     }
-  }
-  
-  document.getElementById('addRowButton').addEventListener('click', addRow);
+
+    // Event listener for technology selection
+    newRow.querySelector('.teknikselect').addEventListener('change', function() {
+        var selectedTech = this.value;
+        // Check if the selected technology is already present in the list
+        if (selectedTechnologies.includes(selectedTech)) {
+            alert("This technology has already been selected in another row.");
+            this.value = ''; // Clear the selection
+        } else {
+            // Update the list of selected technologies
+            selectedTechnologies.push(selectedTech);
+        }
+    });
+}
+
+document.getElementById('addRowButton').addEventListener('click', addRow);
