4e325125de6ef82360174c9babc7af9d9dbacd6b
Antpapa99 <anthony.oram@outlook.com>
2024-04-08 14:48:40 +0200

    now url routing should be based on url names instead of ids

--- alternativkostnaderApp/urls.py
+++ alternativkostnaderApp/urls.py
@@ -5,9 +5,7 @@ urlpatterns = [
     path("", views.home),
     path("data/", views.datainputpage),
     path('commune/', views.CommuneList.as_view()),
-    path('commune/<int:commune_id>', views.CommuneDetail.as_view(), name = "commune_detail"),
-    path('commune/<str:commune_name>', views.RedirectCommuneDetail.as_view(), name='redirect_commune_detail'),
-    path('commune/<int:commune_id>/<str:tech_name>', views.TechDetail.as_view()),
-    path('commune/<str:commune_name>/<str:tech_name>', views.RedirectTechDetail.as_view(), name ='tech_detail')
+    path('commune/<str:commune_name>', views.CommuneDetail.as_view()),
+    path('commune/<str:commune_name>/<str:tech_name>', views.TechDetail.as_view()),
 ]
 
