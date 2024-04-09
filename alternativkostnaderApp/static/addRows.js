591e7124a1775adbf736d2c605cb2f695c7de844
Antpapa99 <anthony.oram@outlook.com>
2024-04-09 17:16:35 +0200

    Should now clear fields when selecting a new commune

--- alternativkostnaderApp/static/addRows.js
+++ alternativkostnaderApp/static/addRows.js
@@ -1,22 +1,28 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function() {
     let table = document.getElementById('tekniktable').getElementsByTagName('tbody')[0];
     let options = ['Digitala lås', 'Digital tillsyn (dag)', 'Digital tillsyn (natt)', 'Läkemedelsrobot', 'Digitalt larm (GPS)', 'Digitalt larm (trygghet)', 'Fallprevention'];
 
     options.forEach(function(optionText) {
-        let newRow = table.insertRow(table.rows.length);
-        let cell = newRow.insertCell(0);
+        let newRow = document.createElement('tr');
+
+        let cell = document.createElement('td');
         cell.textContent = optionText;
+        newRow.appendChild(cell);
         
         // Adding empty cells for other data
         for (let i = 1; i < 5; i++) {
-            let cell = newRow.insertCell(i);
+            let cell = document.createElement('td');
             let input = document.createElement('input');
             input.type = 'text';
             input.classList.add(['installationer', 'minstallationer', 'kinstallation', 'binstallationsek'][i - 1]);
             cell.appendChild(input);
+            newRow.appendChild(cell);
         }
+
+        table.appendChild(newRow);
     });
-};
+    
+});
 
 
 document.getElementById("deleteRowButton").addEventListener('click', rowDeleteFunction)
