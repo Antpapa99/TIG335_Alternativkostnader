c461b699e823e97d2dde28a5df974bb26e192485
Antpapa99 <anthony.oram@outlook.com>
2024-04-09 16:36:41 +0200

    fixed one minor thing with urls

--- alternativkostnaderApp/static/showData.js
+++ alternativkostnaderApp/static/showData.js
@@ -30,7 +30,7 @@ async function getCommuneData() {
 
 async function showCommune(communeName) {
     try {
-        const response = await fetch(`http://127.0.0.1:8000/commune/${communeName}`);
+        const response = await fetch(`https://tig335-alternativkostnader.onrender.com/commune/${communeName}`);
         if (!response.ok) {
             throw new Error(`Failed to fetch data for commune ${communeName}`);
         }
